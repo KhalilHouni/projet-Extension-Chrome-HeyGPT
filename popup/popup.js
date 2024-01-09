dc357b03d0601af948f2d81c95a4454fa8081224
z0opreme <remy.engassac@icloud.com>
2024-01-09 10:19:58 +0100

    front is done we will see later if it need some updates

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,10 @@
+var userQuestion = "";
+const gptAnswer = "prout";
+const buttonSend = document.getElementById('button-send');
+const inputQuestion = document.getElementById('user-question');
+
+
+// Squeeze the mic button
 document.getElementById('checkbox').addEventListener('change', function() {
 	if (this.checked) {
 	  document.querySelector('.switch').style.transform = 'scale(1.2)';
@@ -9,3 +16,14 @@ document.getElementById('checkbox').addEventListener('change', function() {
 	  document.querySelector('.switch').style.transform = 'scale(1)';
 	}
 });
+
+
+// Get the user question from the input when button send is clicked
+// Then after clicking the button show the user question on the mid div
+buttonSend.addEventListener('click', function() {
+	console.log("prout");
+    userQuestion = inputQuestion.value;
+	inputQuestion.value = "";
+	document.getElementById('mid').innerHTML = userQuestion;
+});
+
