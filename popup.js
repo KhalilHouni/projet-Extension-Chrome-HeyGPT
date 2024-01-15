6793ea1dc4aa2ff2e42cf843c40f919bbdacb439
z0opreme <remy.engassac@icloud.com>
2024-01-15 09:42:00 +0100

    re arrange functions order

--- popup.js
+++ popup.js
@@ -5,13 +5,24 @@ const voiceControlCheckbox = document.getElementById('voice-control-checkbox');
 const deleteButton = document.getElementById('delete-button');
 const micCheckbox = document.getElementById('mic-checkbox');
 const micSwitch = document.getElementById('switch');
-const API_KEY = 'APIKEY';
+const GPT_API_KEY = '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';
 const URL = 'https://api.openai.com/v1/completions';
 const weatherApiKey = "23e05a7ea147f7645052bf0de2fd3fa3";
 const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 let synthesis;
 let isVoiceEnabled = true; // Set bot's voice to "off" by default
 
+
+// --------- Buttons PopUP Functions --------- //
+
+// When the page is loaded print welcome message
+document.addEventListener('DOMContentLoaded', function() {
+    var welcome = document.createElement('p');
+    welcome.textContent = "Bienvenue, je suis Hey GPT. Posez moi n'importe quelle question, oralement ou textuellement avec les boutons dédiés en dessous. Je ferais la meilleure réponse possible. J'ai été fait avec ❤️ par Khalil, Maud et Rémy.";
+	var gptTag = createGptTag();
+	convArea.appendChild(gptTag);
+	convArea.appendChild(welcome);
+});
 
 // When the mic checkbox is check scale it
 micCheckbox.addEventListener('change', function() {
@@ -24,29 +35,26 @@ micCheckbox.addEventListener('change', function() {
     }
 });
 
-document.getElementById("test").addEventListener("click", function() {
-	chrome.permissions.request({
-        permissions: ['audioCapture']
-    }, function(granted) {
-        if (granted) {
-            console.log("Permission granted for audioCapture");
-        } else {
-            console.log("Permission denied for audioCapture");
-        }
-    });
+// Function to delete conversation when delete button is clicked
+deleteButton.addEventListener('click', function() {
+	stopAudio();
+    clearConversation();
 });
 
+// Function to clear conversation
+function clearConversation() {
+    convArea.innerHTML = "";
+}
 
-
-// When the page is loaded print welcome message
-document.addEventListener('DOMContentLoaded', function() {
-    var welcome = document.createElement('p');
-    welcome.textContent = "Bienvenue, je suis Hey GPT. Posez moi n'importe quelle question, oralement ou textuellement avec les boutons dédiés en dessous. Je ferais la meilleure réponse possible. J'ai été fait avec ❤️ par Khalil, Maud et Rémy.";
-	var gptTag = createGptTag();
-	convArea.appendChild(gptTag);
-	convArea.appendChild(welcome);
+// Event listener for stop audio button
+document.getElementById('stop-audio').addEventListener('click', function() {
+    stopAudio();
 });
 
+// Function to stop the audio
+function stopAudio() {
+    window.speechSynthesis.cancel();
+}
 
 // Event listener for voice control checkbox
 voiceControlCheckbox.addEventListener('change', function() {
@@ -57,6 +65,11 @@ voiceControlCheckbox.addEventListener('change', function() {
         stopSpeechSynthesis();
     }
 });
+
+
+
+
+// -------- Speech Synthesis Functions -------- //
 
 function startSpeechSynthesis() {
     if (!synthesis) {
@@ -78,40 +91,60 @@ function stopSpeechSynthesis() {
     }
 }
 
-// Stop speech synthesis
-function stopSpeechSynthesis() {
-    synthesis.stop();
-}
+
+
+// ---------- Talk To The Bot Functions ---------- //
 
 // Event listener for send button
 buttonSend.addEventListener('click', function() {
     const userQuestion = inputQuestion.value;
     if (userQuestion) {
         if (shouldPerformGoogleSearch(userQuestion)) {
-            // Respond with "looking on the browser..."
+        	// Respond with "looking on the browser..."
+        	const lookingMessage = createLookingMessage();
+        	appendToConversation(lookingMessage);
+
+        	// Disable ChatGPT
+        	stopSpeechSynthesis();
+
+        	// Perform the Google search
+        	performGoogleSearch(userQuestion);
+		} else if (shouldPerformGoogleImagesSearch(userQuestion)) {
+			// Respond with "looking on the browser..."
             const lookingMessage = createLookingMessage();
             appendToConversation(lookingMessage);
 
             // Disable ChatGPT
             stopSpeechSynthesis();
 
-            // Perform the Google search
-            performGoogleSearch(userQuestion);
-        } else {
-            // If the user's input does not contain "google", proceed with the ChatGPT interaction as before
-           if (userQuestion.includes("weather")) {
-            const location = userQuestion.replace("weather","").trim();
-            getWeatherInfo(location);
-            ;
-           } else { 
-           return ("error");}
-        }
-    }
+            // Perform the Google Images search
+            performGoogleImagesSearch(userQuestion);
+		} else if (userQuestion.includes("weather")) {
+            const location = userQuestion.replace("weather", "").trim();
+        	getWeatherInfo(location);
+        } else { 
+        	triggerChatGPT(userQuestion);
+		}
+    } else {
+		const errorMessage = createErrorMessage();
+		appendToConversation(errorMessage);
+	}
 });
+
+
+
+
+
+/// --------------- Google Search Functions --------------- ///
 
 // Function to check if the user wants to perform a Google search
 function shouldPerformGoogleSearch(userQuestion) {
     return userQuestion.toLowerCase().includes('search on google') && !shouldPerformGoogleImagesSearch(userQuestion);
+}
+
+// Function to perform the Google search with the actual query
+function performGoogleSearch(query) {
+    chrome.runtime.sendMessage({ action: 'performGoogleSearch', query: query });
 }
 
 // Function to check if the user wants to perform a Google Images search
@@ -123,10 +156,9 @@ function shouldPerformGoogleImagesSearch(userQuestion) {
 function performGoogleImagesSearch(query) {
     const searchKeyword = 'search on google pictures of';
     const searchQuery = query.substring(searchKeyword.length).trim(); // Extract the actual query part after "search on google pictures of"
-    stopSpeechSynthesis(); // Stop the bot response
+    stopSpeechSynthesis();
     chrome.runtime.sendMessage({ action: 'performGoogleImagesSearch', query: searchQuery });
 }
-
 
 // Function to create "looking on the browser..." message
 function createLookingMessage() {
@@ -135,21 +167,39 @@ function createLookingMessage() {
     return lookingMessage;
 }
 
-// Function to perform the Google search with the actual query
-function performGoogleSearch(query) {
-    chrome.runtime.sendMessage({ action: 'performGoogleSearch', query: query });
+// Append in conv area error message
+function createErrorMessage() {
+	const errorMessage = document.createElement('p');
+	errorMessage.textContent = "An Error has occured. Please try again.";
 }
 
 
 
-// Function to stop speech synthesis
-function stopSpeechSynthesis() {
-    if (synthesis) {
-        synthesis.stop();
-    }
+
+/// --------------- WeatherApi Functions --------------- ///
+
+function getWeatherInfo(location) {
+    fetch(`${weatherApiUrl}${location}&appid=${weatherApiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            const weatherDescription = data.weather[0].description;
+            const temperature = data.main.temp;
+            const weatherInfo = `Weather in ${location}: ${weatherDescription}, Temperature: ${temperature}°C`;
+            console.log(weatherInfo);
+            const answerWeather = document.createElement('p');
+            answerWeather.textContent = weatherInfo;
+            convArea.appendChild(answerWeather);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
 }
 
 
+
+
+
+/// --------------- ChatGPT Functions --------------- ///
 
 // Function to trigger ChatGPT with user's input and selected language
 async function triggerChatGPT(userInput) {
@@ -173,21 +223,6 @@ async function triggerChatGPT(userInput) {
     scrollToBottom();
 }
 
-////////////////////////////////////
-// Event listener for stop audio button
-document.getElementById('stop-audio').addEventListener('click', function() {
-    stopAudio();
-});
-
-// Function to stop the audio
-function stopAudio() {
-    window.speechSynthesis.cancel();
-}
-
-// Function to clear conversation
-function clearConversation() {
-    convArea.innerHTML = "";
-}
 
 // Function to create user tag
 function createUserTag() {
@@ -225,7 +260,7 @@ function scrollToBottom() {
 async function askQuestion(userQuestion) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${API_KEY}`,
+        'Authorization': `Bearer ${GPT_API_KEY}`,
     };
 
     const body = JSON.stringify({
@@ -278,49 +313,3 @@ function playBotResponse(responseText, language) {
     utterance.lang = selectedLanguage; // Set the language for the speech synthesis
     window.speechSynthesis.speak(utterance);
 }
-
-// Function to delete conversation when delete button is clicked
-deleteButton.addEventListener('click', function() {
-	stopAudio();
-    clearConversation();
-});
-
-function getWeatherInfo(location) {
-    fetch(`${weatherApiUrl}${location}&appid=${weatherApiKey}`)
-        .then(response => response.json())
-        .then(data => {
-            const weatherDescription = data.weather[0].description;
-            const temperature = data.main.temp;
-            const weatherInfo = `Weather in ${location}: ${weatherDescription}, Temperature: ${temperature}°C`;
-            console.log(weatherInfo);
-            const answerWeather = document.createElement('p');
-            answerWeather.textContent = weatherInfo;
-            convArea.appendChild(answerWeather);
-            //appendToConversation(weatherInfo);
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            //appendToConversation('Sorry, an error occurred while fetching weather information.');
-        });
-}
-
-// Function to trigger Google search query using fully-formed http URL
-function googleForAnswers(url) {
-    const functionCall = {
-        name: "google_for_answers",
-        arguments: {
-            url: url
-        }
-    };
-
-    const message = {
-        role: "assistant",
-        content: null,
-        function_call: functionCall
-    };
-
-    // Send the message to the assistant for Google search
-    // (you can use your specific way of sending message to the assistant here)
-}
-
-
